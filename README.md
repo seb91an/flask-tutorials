13da51620196f74a799fa1669d5e184208d52a75
Sebastian Gries <s91gries@gmail.com>
2020-04-16 11:14:56 +0200

    YT CS Tut: Forms added

--- README.md
+++ README.md
@@ -8,5 +8,4 @@ This repo contains code from various Flask tutorials.
 
 * [Official Flask Website](https://flask.palletsprojects.com/en/1.1.x/)
 * [YouTube: Flask Tutorial by Corey Schafer](https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH)
-
-
+* [GitHub: Flask Tutorial by Corey Schafer - Snipptes](https://github.com/CoreyMSchafer/code_snippets/tree/master/Python/Flask_Blog)
--- flaskblog/flaskblog.py
+++ flaskblog/flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8272cad145829be822bb95f1afa0deef'
 
 posts = [
     {
@@ -24,6 +28,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for user {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Check your username or password!', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
